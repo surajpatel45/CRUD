176b2f14cda0733f6996e6940335050348045714
Suraj Patel <surajpatel.cse@gmail.com>
2024-04-23 12:45:10 +0530

    Changed add product form to Reactive form

--- src/app/Component/product/add-product/add-product.component.ts
+++ src/app/Component/product/add-product/add-product.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Product } from '../../../Model/product';
 import { Router } from '@angular/router';
 import { ProductService } from '../../../Service/product.service';
@@ -13,23 +13,45 @@ import { UpperCasePipe } from '@angular/common';
 })
 export class AddProductComponent {
 
-  product: Product = new Product;
+  productForm: FormGroup; // Define FormGroup instance
 
-  constructor(private router: Router, private productService: ProductService, private toastr: ToastrService){}
-
-  saveProduct() {
-    this.productService.addProduct(this.product).subscribe(data => {
-      //console.log(data);
-      this.toastr.success('Product added succesfully','Success');
-      this.router.navigate(['/product/display-products']);
-    },
-    error => {
-      console.log(error);
-      this.toastr.error('Failed to add product', 'Error');
+  constructor(private router: Router, private productService: ProductService, private toastr: ToastrService, private formBuilder: FormBuilder) {
+    this.productForm = this.formBuilder.group({
+      id: ['', Validators.required],
+      name: ['', Validators.required],
+      description: [''],
+      unitPrice: ['', Validators.required],
+      quantity: ['', Validators.required]
     });
   }
-  onSubmit() {
-    console.log(this.product);
-    this.saveProduct();
+
+  saveProduct(): void {
+    const productData: Product = {
+      id: this.productForm.value.id,
+      name: this.productForm.value.name,
+      description: this.productForm.value.description,
+      unitPrice: this.productForm.value.unitPrice,
+      quantity: this.productForm.value.quantity
+    };
+
+    this.productService.addProduct(productData).subscribe(
+      data => {
+        this.toastr.success('Product added successfully', 'Success');
+        this.router.navigate(['/product/display-products']);
+      },
+      error => {
+        console.log(error);
+        this.toastr.error('Failed to add product', 'Error');
+      }
+    );
+  }
+
+
+  onSubmit(): void {
+    if (this.productForm.valid) {
+      this.saveProduct();
+    } else {
+      this.toastr.error('Please fill all required fields', 'Error');
+    }
   }
 }
